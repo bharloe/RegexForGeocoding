8011a79c836fe43eaa0d6a5bbd3ad1e3bf0458f4
BenH <BenH@SDGIS.AMAZONWORKSPACES.COM>
2016-07-21 15:34:23 -0700

    Started writing regex

--- regex.ps1
+++ regex.ps1
@@ -1,6 +1,21 @@
 ﻿(Get-Content .\RegexInput.txt) |
 
-#Start
-Foreach-Object {$_ -replace "\sDE$",""}|
+#Removing directional text at beginning of string
+Foreach-Object {$_ -replace "^1\s\/.\/.\/.\s",""}|
+Foreach-Object {$_ -replace "^1\s.\/.\/.\/.\s",""}|
+Foreach-Object {$_ -replace "^1\s.\/.\/.\s",""}|
+Foreach-Object {$_ -replace "^1\s.\/.\s",""}|
+Foreach-Object {$_ -replace "^1\/.\/.\s",""}|
+
+#Removing directional text in middle of string
+Foreach-Object {$_ -replace "\s\/.\/.\/.\s"," AND "}|
+Foreach-Object {$_ -replace "\s.\/.\/.\s"," AND "}|
+Foreach-Object {$_ -replace "\s.\/.\s"," AND "}|
+
+#Remove unnecessary textual descriptions
+Foreach-Object {$_ -replace "\@\sT\-INTERSECTION\sOF\s",""}|
+Foreach-Object {$_ -replace "\sMedian\sOn\s"," "}|
+Foreach-Object {$_ -replace "\sMedian\s"," "}|
+Foreach-Object {$_ -replace "ACCESS\sRD$"," "}|
 
 Set-Content .\RegexOutput.txt
